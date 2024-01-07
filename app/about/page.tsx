995b46aa6f1ab6a973f8b7bf729845df0aed0590
kelani34 <kelanitaiwo34@gmail.com>
2024-01-07 20:45:00 +0100

    fix: deployment bug

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,18 +1,19 @@
 'use client';
 
-import Container from '@/components/container';
-import AboutHeader from '@/containers/about-header';
-import Journey from '@/containers/journey';
-import Mission from '@/containers/mission';
+// import Container from '@/components/container';
+// import AboutHeader from '@/containers/about-header';
+// import Journey from '@/containers/journey';
+// import Mission from '@/containers/mission';
 
 const About = () => {
   return (
-    <Container>
-      <AboutHeader />
-      <Journey />
-      <Mission />
-      {/* <Team /> */}
-    </Container>
+    // <Container>
+    //   <AboutHeader />
+    //   <Journey />
+    //   <Mission />
+    //   {/* <Team /> */}
+    // </Container>
+    <></>
   );
 };
 
